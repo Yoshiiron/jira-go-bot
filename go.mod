f173fb712959e37ea9cda2e6a3e858e2cec4c133
denis.astafev <denis.astafev@devim.team>
2024-02-27 19:04:44 +0300

    0.0.2 - Sending messages on denis.astafev@devim.team

--- go.mod
+++ go.mod
@@ -12,13 +12,17 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.17.2 // indirect
 	github.com/nats-io/nats.go v1.33.1 // indirect
 	github.com/nats-io/nkeys v0.4.7 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rusq/dlog v1.3.3 // indirect
+	github.com/rusq/tbcomctl/v4 v4.0.1 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
 	github.com/trivago/tgo v1.0.7 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/rusq/tbcomctl/v4"
 	tele "gopkg.in/telebot.v3"
-	jiraFuncs "jira-go-bot/JiraFuncs"
+	"jira-go-bot/Sender"
 	"log"
+	"math/rand"
 	"strings"
 	"time"
 )
+
+func randomizer() string {
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		"0123456789")
+	length := 8
+	var build strings.Builder
+	for i := 0; i < length; i++ {
+		build.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	str := build.String()
+
+	return str
+}
+
+func UserRegistration(b *tele.Bot) {
+	codeInput := tbcomctl.NewInputText("code", "Пожалуйста, введите код отправленный вам на mail.yandex.ru", processInput(b))
+	form := tbcomctl.NewForm(codeInput)
+
+	b.Handle("/jira_acc", form.Handler)
+	str := randomizer()
+	Sender.SendMessage(str)
+
+	b.Handle(tele.OnText, func(c tele.Context) error {
+		if c.Message().IsReply() {
+			if c.Message().Text == str {
+				c.Send("Прекрасно.")
+			}
+		}
+		return nil
+	})
+}
 
 func main() {
 
@@ -22,22 +58,29 @@ func main() {
 		return
 	}
 
-	b.Handle("/jira_acc", func(m tele.Context) error {
-		message := m.Message().Text
-		parts := strings.Fields(message)
-		fmt.Println(parts[1])
-		user := jiraFuncs.UserKeyReturner(jiraFuncs.JiraClient(), parts[1])
-		if user == "" {
-			m.Send("Такого пользователя не сущесвует, повторите попытку.")
-		} else {
-			m.Send(fmt.Sprintf("Key: %v, TelegramUID: %v", user, m.Sender().ID))
-		}
-		return nil
-	})
-
+	UserRegistration(b)
+	//
 	b.Handle("/start", func(m tele.Context) error {
 		m.Send("Привет!\nЯ Джира-Бот, призван помочь твоей работе внутри Devim!\nЯ буду присылать тебе задачки из Jira прямо в телеграмм.")
 		return nil
 	})
+
 	b.Start()
+}
+
+func processInput(b *tele.Bot) func(ctx context.Context, c tele.Context) error {
+	return func(ctx context.Context, c tele.Context) error {
+		val := c.Message().Text
+		log.Println("msgCb function is called, input value:", val)
+		switch val {
+		case "error":
+			return fmt.Errorf("error requested: %s", val)
+		case "wrong":
+			return tbcomctl.NewInputError("wrong input")
+		}
+		if ctrl, ok := tbcomctl.ControllerFromCtx(ctx); ok {
+			log.Println("form values so far: ", ctrl.Form().Data(c.Sender()))
+		}
+		return nil
+	}
 }
